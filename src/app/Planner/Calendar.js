f82bbe2e8ac7f0b39c383b14bb37bf49be48b9a1
Jaron <jjkenyon777@gmail.com>
2023-05-12 23:08:33 -0700

    calendar ui started

--- src/app/Planner/Calendar.js
+++ src/app/Planner/Calendar.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
 
 function Calendar() {
+  const days = ['S', 'M', 'T', 'W', 'T', 'F', 'S'] ;
+  const dates = Array.from({ length: 30 }, (_, i) => i + 1); // for a 30 day month
+
   return (
-    <div>Calendar</div>
-  )
+    <div className="flex flex-col items-center sm:p-4 ">
+      <h2 className="text-xl text-black font-bold mb-4">May 2023</h2>
+      <div className="grid grid-cols-7 text-black w-full">
+        {days.map((day, index) => (
+          <div key={index} className="text-center font-bold">
+            {day}
+          </div>
+        ))}
+        {dates.map((date, index) => (
+          <div key={index} className="text-center w-full h-28 mx-auto border rounded p-2">
+            {date}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Calendar
+export default Calendar;
--- src/app/Planner/page.js
+++ src/app/Planner/page.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import Calendar from './Calendar'
 
 function page() {
   return (
-    <div>page</div>
+    <div className="flex justify-center items-center min-h-screen sm:p-4 md:p-0">
+    <div className="bg-white shadow sm:p-4 rounded w-full md:max-w-lg sm:mx-auto pb-10">
+    <Calendar />
+  </div>
+</div>
+
   )
 }
 
