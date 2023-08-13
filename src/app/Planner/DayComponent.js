393e6ea79395479aa1a53a6fa316291f53d32cde
Jaron15 <jjkenyon777@gmail.com>
2023-08-12 17:12:32 -0700

    react icons

--- src/app/Planner/DayComponent.js
+++ src/app/Planner/DayComponent.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import DayViewModal from './DayViewModal';
 import { useState, useEffect} from 'react';
+import { useSelector } from 'react-redux';
 
 
 const DayComponent = ({ day, isWeekend, isDifferentMonth, events = [], hobbies = [], tasks = [], currentMonth, currentYear, formattedMonth}) => {
@@ -9,11 +10,15 @@ const DayComponent = ({ day, isWeekend, isDifferentMonth, events = [], hobbies =
   const [forceUpdate, setForceUpdate] = useState(false);
   const formattedDay = String(day).padStart(2, '0');
   const date = `${currentYear}-${formattedMonth}-${formattedDay}`;
+  const completedTasks = useSelector((state) => state.tasks.completedTasks);
+    // const tasks = useSelector((state) => state.tasks.tasks);
 
   const handleClose = () => {
     console.log('handleClose');
     setModalOpen(false)
   }
+
+  const completedTaskClass = '!line-through !bg-slate-500  !border-slate-500 dark:!border-slate-500 !shadow-slate-500 !dark:shadow-slate-500 !text-slate-400 dark:!text-slate-900 dark:!bg-slate-500';
 
     let tdClassNames = "ease relative  cursor-pointer border border-stroke sm:p-1 p-[5px] transition duration-500 hover:bg-gray dark:border-strokedark dark:hover:bg-meta-4 md:p-3 xl:h-31 overflow-clip text-center sm:text-start";
     let spanClassNames = "font-medium text-black dark:text-white ";
@@ -37,21 +42,29 @@ const DayComponent = ({ day, isWeekend, isDifferentMonth, events = [], hobbies =
     className={tdClassNames}
     onClick={() => setModalOpen(true)}>
       <span className={spanClassNames}>{day}</span>
-      {hobbies && hobbies.length > 0 && hobbies.map((hobby, i) => (
-        <div key={i} className="dark:bg-green-800 text-black dark:text-white border border-green-600 shadow shadow-green-600 dark:shadow-none dark:border-none px-0.5 sm:py-0.5 py-[1px] rounded sm:mt-1 overflow-clip whitespace-nowrap  sm:text-base text-[0.5rem]">
+      {hobbies && hobbies.length > 0 && hobbies.map((hobby, i) => {
+        const hasPracticeLog = hobby.practiceLog && hobby.practiceLog.some(log => log.date === date);
+
+        return (
+        <div key={i} className={`dark:bg-green-800 text-black dark:text-white border border-green-600 shadow shadow-green-600 dark:shadow-none dark:border-none px-0.5 sm:py-0.5 py-[1px] rounded sm:mt-1 overflow-clip whitespace-nowrap  sm:text-base text-[0.5rem] ${hasPracticeLog ? 'bg-slate-500 dark:bg-slate-500 border-slate-500 dark:border-slate-500 shadow-slate-500 dark:shadow-slate-500 text-slate-400 dark:!text-slate-900 line-through' : ''}`}>
           {hobby.name}
         </div>
-      ))}
+      )})}
       {events && events.length > 0 && events.map((event, i) => (
         <div key={i} className="dark:bg-blue-800 text-black dark:text-white border-blue-400 border shadow dark:shadow-none dark:border-none shadow-blue-400 px-0.5 sm:py-0.5 py-[1px] rounded sm:mt-1 overflow-clip whitespace-nowrap  sm:text-base text-[0.5rem]">
           {event.name}
         </div>
       ))}
-      {tasks && tasks.length > 0 && tasks.map((task, i) => (
-  <div key={i} className="dark:bg-yellow-800 text-black dark:text-white border dark:border-none dark:shadow-none border-yellow-600 shadow shadow-yellow-600 px-0.5 sm:py-0.5 py-[1px] rounded sm:mt-1 overflow-clip whitespace-nowrap  sm:text-base text-[0.5rem]">
+      {tasks && tasks.length > 0 && tasks.map((task, i) => { 
+            const isRecurringTaskCompleted = completedTasks && completedTasks.some(ctask => ctask.refId === task.refId && (ctask.dueDate === date || ctask.completedDate === date));
+            const isSingularTaskCompleted = task.isCompleted && task.dueDate === date;
+            const isCompleted = isRecurringTaskCompleted || isSingularTaskCompleted;
+
+        return(
+  <div key={i} className={`dark:bg-yellow-800 text-black dark:text-white border dark:border-none dark:shadow-none border-yellow-600 shadow shadow-yellow-600 px-0.5 sm:py-0.5 py-[1px] rounded sm:mt-1 overflow-clip whitespace-nowrap sm:text-base text-[0.5rem] ${isCompleted ? completedTaskClass : ''}`}>
     {task.name}
   </div>
-))}
+)})}
 <DayViewModal 
   isOpen={isModalOpen} 
   onClose={handleClose} 
--- src/app/Planner/DayViewModal.js
+++ src/app/Planner/DayViewModal.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import LogPracticeForm from "./LogPracticeForm";
 import { useDispatch, useSelector } from 'react-redux';
 import { addCompletedTask, deleteCompletedTask, markTaskAsCompleted, updateTask } from '../../redux/tasks/tasksSlice';
+import { FaPencilAlt, FaTrash } from 'react-icons/fa'; 
+import {ImCancelCircle} from 'react-icons/Im'
 
 const DayViewModal = ({ isOpen, onClose, items, date }) => {
   const dispatch = useDispatch();
@@ -17,7 +19,7 @@ console.log(date);
       case "Event":
         return "border border-blue-400 shadow-blue-400";
       case "Hobby":
-        return "border border-green-600 shadow-green-600";
+        return "border border-green-800 shadow-green-600";
       case "Task":
         return "border border-yellow-600 shadow-yellow-600";
       default:
@@ -69,30 +71,50 @@ const completedTask = completedTasks.find(ctask => ctask.refId === task.id && ct
 
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50 w-full lg:w-768px mx-auto">
-  <div className="bg-white w-5/6 h-5/6 sm:w-3/5 sm:h-4/6 md:w-4/6 md:h-4/6 lg:w-1/2 2xl:w-2/5 lg:ml-72 lg:mt-14 mt-12 overflow-y-scroll rounded-lg p-6 bg-white dark:bg-boxdark border border-primary hide-scrollbar">
-        <button
-          onClick={(event) => {
+  <div className="relative bg-white w-5/6 h-5/6 sm:w-3/5 sm:h-4/6 md:w-4/6 md:h-4/6 lg:w-1/2 2xl:w-2/5 lg:ml-72 lg:mt-14 mt-12 overflow-y-scroll rounded-lg p-6 bg-white dark:bg-boxdark border border-primary hide-scrollbar">
+    <div className="w-full flex mb-12">
+          {/* <div 
+            // onClick={handleEdit} 
+            className="absolute top-5 left-6 cursor-pointer text-black dark:text-current"
+            title="Edit"
+        >
+           <FaPencilAlt size={24} />
+        </div> */}
+
+        <div 
+           onClick={(event) => {
             event.stopPropagation();
             onClose();
           }}
-          className="mb-4 bg-red-500 text-black dark:text-white"
+            className="absolute top-5 right-6 cursor-pointer cursor-pointer text-black dark:text-current"
+            title="Close"
         >
-          Close
-        </button>
+            <ImCancelCircle size={24} />
+        </div>
+        </div>
         {items.map((item, index) => {
       const isRecurringTaskCompletedForDate  = completedTasks && completedTasks.some(ctask => ctask.refId === item.refId && (ctask.dueDate === date || ctask.completedDate === date));
       const isSingularTaskCompletedForDate = tasks && tasks.some(task => task.refId === item.refId && task.isCompleted && task.dueDate === date);
+      const hasPracticeLog = item.practiceLog && item.practiceLog.some(log => log.date === date);
 
           return (
-  <div key={index} className="mb-4 ">
+
+  <div key={index} className="mb-4 relative">
+ {expandedItem === index && (
+          <>
+          <FaTrash className="absolute top-6 right-5 cursor-pointer dark:!text-white z-50" onClick={() => handleDelete(item)} />
+<FaPencilAlt style={{ color: 'red !important' }} className="absolute top-6 left-5 cursor-pointer dark:!text-white z-50" onClick={() => handleEdit(item)} />
+
+          </>
+        )}
     <div
       className={`p-4 rounded-lg dark:bg-black  shadow drop-shadow   ${getClassNameForCategory(
         item.category
-      )}`}
+      )} ${isRecurringTaskCompletedForDate || isSingularTaskCompletedForDate || hasPracticeLog ? '!text-slate-600 dark:!text-slate-600 !border-slate-500 !shadow-slate-500' : ''}`}
     >
       <span 
       onClick={() => toggleDetails(index)}
-      className="text-black dark:text-white text-center font-bold block text-xl ">{item.name}</span>
+      className={`text-black dark:text-white text-center font-bold block text-xl ${isRecurringTaskCompletedForDate || isSingularTaskCompletedForDate ? 'text-slate-600 dark:text-slate-300' : ''}`}>{item.name}</span>
               {expandedItem === index && (
   <div className=" bg-gray-100 p-4 rounded-md ">
     {/* EVENT */}
@@ -138,9 +160,9 @@ const completedTask = completedTasks.find(ctask => ctask.refId === task.id && ct
               />
             </li>
             {(item.type === 'recurring' && isRecurringTaskCompletedForDate) || (item.type !== 'recurring' && isSingularTaskCompletedForDate) ? (
-            <button onClick={() => handleUndo(item, date)}>Undo Completion</button>
+            <button className="border border-yellow-600 rounded p-2 mt-2 hover:shadow hover:shadow-yellow-600" onClick={() => handleUndo(item, date)}>Undo Completion</button>
           ) : (
-            <button onClick={() => handleCompletion(item, date)}>Mark as Complete</button>
+            <button className="border border-yellow-600 rounded p-2 mt-2 hover:shadow hover:shadow-yellow-600" onClick={() => handleCompletion(item, date)}>Mark as Complete</button>
           )}
       </ul>
     )}
