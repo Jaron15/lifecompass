6124a43eb89a2fa1a0fbda519d7ffa8ee2bb0256
Jaron15 <jjkenyon777@gmail.com>
2023-09-24 18:18:58 -0700

    TimeSpent component

--- src/components/THE/hobbyComponents/notebook/overview/Overview.js
+++ src/components/THE/hobbyComponents/notebook/overview/Overview.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import HobbyStreak from './HobbyStreak'
+import TimeSpent from './TimeSpent'
 
 function Overview({hobby}) {
   return (
     <div>
         <HobbyStreak hobby={hobby} />
+        <div className='flex justify-center w-full w-4/5'>
+        <TimeSpent hobby={hobby} />
+        </div>
     </div>
   )
 }
---
+++ src/components/THE/hobbyComponents/notebook/overview/TimeSpent.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function TimeSpent({ hobby }) {
+  const accumulatedTime = hobby.practiceLog.reduce((total, log) => total + log.timeSpent, 0);
+
+  const hours = Math.floor(accumulatedTime / 60);
+  const minutes = accumulatedTime % 60;
+
+  return (
+    <div className='w-4/5 flex justify-center text-lg text-center flex-col'>
+        <div className='w-full font-semibold underline'> Accumulated Time Spent:</div>
+    <div className='w-full'>
+      You have spent a total of {hours} hours {minutes} minutes Practicing!
+    </div>
+    </div>
+  );
+}
+
+export default TimeSpent;
