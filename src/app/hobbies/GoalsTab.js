6d36a191b5a228a1a2cb34a055bc17675cb856c8
Jaron15 <jjkenyon777@gmail.com>
2023-10-07 22:41:50 -0700

    edit goal selector

--- src/app/hobbies/GoalsTab.js
+++ src/app/hobbies/GoalsTab.js
@@ -4,18 +4,62 @@ import React, { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux';
 import { FaPencilAlt } from 'react-icons/fa';
 import EditGoalModal from '@/src/components/THE/hobbyComponents/notebook/goals/EditGoalModal';
+import { FaAngleDown } from "react-icons/fa"; 
 
-
+export function RadioButton({ options, value, onChange }) {
+    return (
+      <div className="flex bg-gray-200 p-1 rounded-md shadow-inner w-72">
+        {options.map((option) => (
+          <label
+            key={option.value}
+            className="flex-1 text-center cursor-pointer"
+          >
+            <input
+              type="radio"
+              name="term"
+              value={option.value}
+              checked={value === option.value}
+              onChange={(e) => onChange(e.target.value)}
+              className="sr-only" 
+            />
+            <span
+              className={`block py-2 rounded-md ${
+                value === option.value
+                  ? "bg-white font-semibold"
+                  : "text-gray-700"
+              }`}
+            >
+              {option.label}
+            </span>
+          </label>
+        ))}
+      </div>
+    );
+  }
+  
 
 function GoalsTab({ hobby }) {
     const {user} = useSelector(state => state.user)
     const [isModalOpen, setIsModalOpen] = useState(false);
-  
+    const [modalTerm, setModalTerm] = useState('both');
+
+    const handleOpenModal = (term) => {
+        setModalTerm(term);
+        setIsModalOpen(true);
+    };
+    const radioOptions = [
+        { value: "both", label: "All Goals" },
+        { value: "short", label: "Short-term" },
+        { value: "long", label: "Long-term" },
+      ];
+    
+    
+    
   const hobbyId = hobby.refId
   const [mobileSort, setMobileSort] = useState('both')
   return (
-    <div className="grid grid-cols-2 gap-4 overflow-auto pb-4">
-        {isModalOpen && <EditGoalModal closeModal={() => setIsModalOpen(false)} term="long" />}
+    <div className="grid grid-cols-2 gap-4  pb-4 ">
+        {isModalOpen && <EditGoalModal closeModal={() => setIsModalOpen(false)} term={modalTerm} hobbyId={hobbyId} />}
       {/* Add Goal Form Box */}
       <div className="col-span-2 bg-gray-200 p-4 rounded-md border border-stroke bg-white dark:border-strokedark dark:bg-boxdark shadow shadow-2xl mb-4">
         <AddGoal user={user} hobbyId={hobbyId} />
@@ -23,8 +67,9 @@ function GoalsTab({ hobby }) {
 
       {/* For larger screens: Short-term and Long-term Goals */}
       <div className="col-span-1 bg-gray-200 p-4 rounded-md border border-stroke bg-white dark:border-strokedark dark:bg-boxdark shadow shadow-2xl hidden sm:block relative h-[26rem]">
-    <FaPencilAlt className="absolute top-4 right-5 text-xl cursor-pointer z-20"
-    onClick={() => setIsModalOpen(true)}
+    <FaPencilAlt 
+    className="absolute top-4 right-5 text-xl cursor-pointer z-20"
+    onClick={() => handleOpenModal('short')}
      />
     <h2 className="text-xl mb-2 text-center sticky top-0 bg-white dark:bg-boxdark z-10 pb-2">Short-term Goals</h2>
     <div className="h-[22rem] overflow-auto hide-scrollbar">
@@ -33,32 +78,38 @@ function GoalsTab({ hobby }) {
 </div>
 
 <div className="col-span-1 bg-gray-200 p-4 rounded-md border border-stroke bg-white dark:border-strokedark dark:bg-boxdark shadow shadow-2xl hidden sm:block relative h-[26rem]">
-    <FaPencilAlt className="absolute top-4 right-5 text-xl cursor-pointer z-20" />
+    <FaPencilAlt 
+    className="absolute top-4 right-5 text-xl cursor-pointer z-20" 
+    onClick={() => handleOpenModal('long')}
+    />
     <h2 className="text-xl mb-2 text-center sticky top-0 bg-white dark:bg-boxdark z-10 pb-2">Long-term Goals</h2>
     <div className="h-[22rem] overflow-auto hide-scrollbar">
         <GoalList hobbyId={hobbyId} term={'long'} 
-        onClick={() => setIsModalOpen(true)}
         />
     </div>
 </div>
 
-      {/* For smaller screens: Combined Goals */}
-      <div className="col-span-2 md:hidden bg-gray-200 p-4 rounded-md border border-stroke bg-white dark:border-strokedark dark:bg-boxdark shadow shadow-2xl sm:hidden h-[26rem]">
-    <div className="mb-2 flex justify-between items-center mb-4 sticky top-0 bg-white dark:bg-boxdark z-10">
-        <h2 className="text-xl text-center">Goals</h2>
-        <select
+ {/* For smaller screens: Combined Goals */}
+ <div className="col-span-2 md:hidden bg-gray-200 p-4 rounded-md border border-stroke bg-white dark:border-strokedark dark:bg-boxdark shadow shadow-2xl sm:hidden h-[26rem] overflow-clip mb-6">
+        <div className="mb-4 sticky top-0 bg-white dark:bg-boxdark z-10 flex justify-center">
+        <div className="absolute top-0 right-0">
+            <button 
+            onClick={() => handleOpenModal(mobileSort)}>
+              <FaPencilAlt />
+            </button>
+          </div>
+          <div className='mt-6'>
+          <RadioButton
+            options={radioOptions}
             value={mobileSort}
-            onChange={(e) => setMobileSort(e.target.value)}
-        >
-            <option value="both">All Goals</option>
-            <option value="short">Short-term</option>
-            <option value="long">Long-term</option>
-        </select>
-    </div>
-    <div className="h-[22rem] overflow-auto hide-scrollbar">
-        <GoalList hobbyId={hobbyId} term={mobileSort} />
-    </div>
-</div>
+            onChange={setMobileSort}
+          />
+          </div>
+        </div>
+        <div className="h-[22rem] overflow-auto hide-scrollbar">
+          <GoalList hobbyId={hobbyId} term={mobileSort} />
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/THE/hobbyComponents/notebook/goals/EditGoalModal.js
+++ src/components/THE/hobbyComponents/notebook/goals/EditGoalModal.js
@@ -1,18 +1,67 @@
+import { RadioButton } from "@/src/app/hobbies/GoalsTab";
 import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import GoalList from "./GoalList";
 
-function GoalSelector({ onSelectGoal, initialTerm }) {
-    const [currentTerm, setCurrentTerm] = useState(initialTerm);
-    // Fetch goals based on currentTerm...
-    // ...
-  
+function GoalSelector({ onSelectGoal, initialTerm, closeModal, hobbyId }) {
+    console.log(hobbyId);
+    const allGoals = useSelector(state => 
+        state.hobbies.hobbies.find(hobby => hobby.refId === hobbyId)?.goals || []
+      );
+console.log(allGoals);
+    const [selectedTerm, setSelectedTerm] = useState(initialTerm);
+
+    // Filter goals based on the term
+    const goals = allGoals.filter(goal => {
+        if (selectedTerm === 'both') return true;
+        return goal.type === selectedTerm;
+    });
+
+    const radioOptions = [
+      { value: "both", label: "All Goals" },
+      { value: "short", label: "Short-term" },
+      { value: "long", label: "Long-term" },
+    ];
+
+    
     return (
-      <div>
-        {/* Dropdown for term selection */}
-        {/* Render list of goals */}
-        {/* On goal click: onSelectGoal(goal) */}
-      </div>
-    );
+        <div className="flex flex-col h-[90%]"> 
+          <div className="flex justify-center sticky top-0  z-10 py-2">
+            <RadioButton
+              options={radioOptions}
+              value={selectedTerm}
+              onChange={setSelectedTerm}
+            />
+          </div>
+          <div className="text-center  text-xl ">
+            Select goal to edit
+        </div>
+          {/* Scrollable List of Goals */}
+          <div className="flex-grow overflow-auto hide-scrollbar">
+            <ul className="flex flex-col items-center w-full py-4">
+              {goals.map((goal) => {
+                const borderColor =
+                  goal.type === "short" ? "border-blue-400" : "border-green-400";
+                return (
+                  <li
+                    className={`border w-full md:w-[36rem] rounded p-2 flex justify-center items-center ${borderColor} my-2 cursor-pointer`}
+                    key={goal.id} onClick={() => onSelectGoal(goal)}
+                  >
+                    {goal.name}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+  
+          {/* Sticky Close Button */}
+          <div className="sticky bottom-0 text-center  z-10 py-2">
+            <button onClick={closeModal}>Close</button>
+          </div>
+        </div>
+      );
   }
+  
   
   function GoalEditor({ goal }) {
       const [goalName, setGoalName] = useState(goal.name);
@@ -35,25 +84,24 @@ function GoalSelector({ onSelectGoal, initialTerm }) {
     );
 }
 
-function EditGoalModal({ closeModal, term }) {
+function EditGoalModal({ closeModal, term, hobbyId }) {
     const [selectedGoal, setSelectedGoal] = useState(null);
-    useEffect(() => {
-        // When the modal mounts, prevent body from scrolling
-        document.body.classList.add('overflow-hidden');
-    
-        // When the modal unmounts, allow body to scroll again
-        return () => {
-          document.body.classList.remove('overflow-hidden');
-        };
-      }, []);
+    const handleCloseModal = () => {
+        setSelectedGoal(null); // Reset selected goal (if any)
+        closeModal(); // Close the modal
+    }
+
     
     return (
       <div className="absolute top-0 left-0 flex w-full h-full items-center justify-center bg-black bg-opacity-50 z-50 ">
       <div className="relative bg-white 2xsm:w-full 2xsm:h-3/4 xsm:w-5/6 xsm:h-3/4 overflow-y-scroll rounded-lg p-6 bg-white dark:bg-boxdark border border-primary hide-scrollbar">
+        <div className="text-center font-semibold text-3xl pb-2 underline">
+            Edit Goal
+        </div>
           {selectedGoal ? (
             <GoalEditor goal={selectedGoal} />
           ) : (
-            <GoalSelector onSelectGoal={setSelectedGoal} initialTerm={term} />
+            <GoalSelector onSelectGoal={setSelectedGoal} initialTerm={term} closeModal={handleCloseModal} hobbyId={hobbyId}/>
           )}
         </div>
       </div>
