52a9abf41e86d9ca90bf02b517980dac36853331
Jaron15 <jjkenyon777@gmail.com>
2023-10-13 20:14:52 -0700

    streak component refactor

--- src/components/streak/StreakCounter.js
+++ src/components/streak/StreakCounter.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Image from 'next/image';
 
 const StreakCounter = ({ count }) => {
     return (
+        <>
+        <Image src="/fire.png" alt="Fire Icon" width={40} height={40} />
         <div className="streak-counter text-2xl font-bold my-4 text-black dark:text-current">
             {count}
         </div>
+        </>
     );
 };
 
--- src/components/streak/StreakTracker.js
+++ src/components/streak/StreakTracker.js
@@ -2,9 +2,6 @@ import React from 'react';
 import DayBubble from './DayBubble';
 import StreakCounter from './StreakCounter';
 import StreakMessage from './StreakMessage';
-import { FaFire } from 'react-icons/fa';
-import Image from 'next/image';
-
 import { isToday, parseISO } from 'date-fns';
 
 const calculateBubbles = (streak, lastUpdatedDate) => {
@@ -46,7 +43,6 @@ console.log(streak);
     return (
         <div className="w-full flex flex-col
         justify-center items-center ">
-          <Image src="/fire.png" alt="Fire Icon" width={40} height={40} />
             <StreakCounter count={streak} />
             <div className="flex justify-center w-4/5 shrink">
                 {["M", "T", "W", "T", "F", "S", "S"].map((dayName, index) => (
