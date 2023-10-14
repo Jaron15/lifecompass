3f53768d55c5f9096f5304a25e02c97f595f19f2
Jaron15 <jjkenyon777@gmail.com>
2023-10-13 21:38:43 -0700

    goal line edit

--- src/components/THE/hobbyComponents/notebook/progress/WeeklyChart.js
+++ src/components/THE/hobbyComponents/notebook/progress/WeeklyChart.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { BarChart, Bar, XAxis, YAxis, Tooltip, CartesianGrid, ResponsiveContainer,ReferenceLine, Label  } from 'recharts';
+import useColorMode from '../../../../../hooks/useColorMode';
 
 
 function WeeklyChart({ data, goal }) {
@@ -21,6 +22,23 @@ function WeeklyChart({ data, goal }) {
         const maxTimeSpent = Math.max(...data.map(item => item.timeSpent));
         return Math.ceil(maxTimeSpent / 10) * 10; 
     }
+    const getCustomTicks = (data, goal) => {
+        const maxValue = getMaxValue(data);
+        const interval = 10; // Adjust this value based on your data range.
+        let ticks = [];
+        for(let i = 0; i <= maxValue; i += interval) {
+            ticks.push(i);
+        }
+        if (ticks.indexOf(goal) === -1) {
+            ticks.push(goal);
+        }
+        return ticks.sort((a, b) => a - b);
+    };
+    
+    // Inside your component render function:
+    const customTicks = getCustomTicks(data, goal);
+    
+    
     const CustomTooltip = ({ active, payload, label }) => {
         if (active && payload && payload.length) {
             return (
@@ -31,7 +49,11 @@ function WeeklyChart({ data, goal }) {
         }
         return null;
     };
+
     
+
+
+      
     return (
         <ResponsiveContainer width="100%" height="100%">
             <BarChart
@@ -40,13 +62,27 @@ function WeeklyChart({ data, goal }) {
                 margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
             >
                 <CartesianGrid strokeDasharray="3 3" horizontal={false} />
-                <XAxis type="number" domain={[0, getMaxValue(data)]} />
+                <XAxis type="number" domain={[0, getMaxValue(data)]} ticks={customTicks} />
+                <XAxis type="number" value={goal} />
                  <YAxis dataKey="name" type="category" width={40} />
                  <Tooltip content={<CustomTooltip />} />
                 <Bar dataKey="timeSpent" fill="#4c9cc7" />
-                <ReferenceLine x={goal} stroke="orange" strokeDasharray="5 5"  >
-                    <Label value={"Goal"} position={"insideBottomLeft"} />
-                </ReferenceLine>
+                <ReferenceLine 
+    x={goal} 
+    stroke="black" 
+    strokeDasharray="6 6"
+    strokeWidth={2}
+    label={{
+        value: 'Goal', 
+        position: 'center', 
+        angle: 270, 
+        fill: 'white', 
+        stroke: 'black', 
+        strokeWidth: 0.6, 
+        fontSize: 16, 
+        fontWeight: "bold",
+    }}
+/>
             </BarChart>
         </ResponsiveContainer>
     );
