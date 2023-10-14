636688cbdaf9335ca01c9acd6d6d7e5f68d2091c
Jaron15 <jjkenyon777@gmail.com>
2023-10-14 12:17:16 -0700

    best streak/ layout fixes

--- src/app/hobbies/ProgressTab.js
+++ src/app/hobbies/ProgressTab.js
@@ -1,7 +1,9 @@
 import WeeklyChart from '@/src/components/THE/hobbyComponents/notebook/progress/WeeklyChart';
+import StreakCounter from '@/src/components/streak/StreakCounter';
 import React from 'react'
 
 function ProgressTab({hobby}) {
+    console.log(hobby.bestStreak);
     const getDayOfWeek = (dateString) => {
         const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
         const date = new Date(dateString);
@@ -40,12 +42,12 @@ function ProgressTab({hobby}) {
         console.log(chartData);
         const goal = hobby.practiceTimeGoal
         return (
-            <div className="grid grid-cols-1 grid-rows-6 sm:grid-cols-2 sm:grid-rows-2 xl:grid-cols-6 xl:grid-rows-6 gap-4 h-3/4 border">
+            <div className="flex flex-col sm:grid sm:grid-cols-2 sm:grid-rows-2 xl:grid-cols-4 xl:grid-rows-6 gap-4 sm:h-3/4 border mb-20 sm:mb-0">
               
               {/* Weekly Activity Graph */}
-              <div className="col-span-1 row-span-5 sm:col-span-1 sm:row-span-1 xl:col-span-3 xl:row-span-6 bg-gray-200 p-4 rounded-md border border-stroke bg-white p-3 dark:border-strokedark dark:bg-boxdark shadow shadow-2xl">
+              <div className="sm:col-span-1 sm:row-span-1 xl:col-span-2 xl:row-span-6 bg-gray-200 p-4 rounded-md border border-stroke bg-white p-3 dark:border-strokedark dark:bg-boxdark shadow shadow-2xl h-90 sm:h-full">
                 <div className='w-full text-center text-lg font-semibold'>
-                Average Daily Dedication Overview
+                Average Daily Dedication
                 </div>
                 {/* Graph Component */}
                 <div className="h-full bg-gray-300 rounded">
@@ -54,18 +56,26 @@ function ProgressTab({hobby}) {
               </div>
           
               {/* Goals/Milestones Completed Box */}
-              <div className="col-span-1 row-span-1 sm:col-span-1 xl:col-span-3 xl:row-span-4 bg-gray-200 p-4 rounded-md border border-stroke bg-white p-3 dark:border-strokedark dark:bg-boxdark shadow shadow-2xl">
+              <div className="sm:col-span-1 xl:col-span-2 xl:row-span-3 bg-gray-200 p-4 rounded-md border border-stroke bg-white p-3 dark:border-strokedark dark:bg-boxdark shadow shadow-2xl">
                 Goals & Milestones Completed
               </div>
           
               {/* Practice Time Goal Achievements Box */}
-              <div className="col-span-1 row-span-1 sm:col-span-1 xl:col-span-3 xl:row-span-1 bg-gray-200 p-4 rounded-md border border-stroke bg-white p-3 dark:border-strokedark dark:bg-boxdark shadow shadow-2xl">
+              <div className="sm:col-span-1 xl:col-span-1 xl:row-span-3 bg-gray-200 p-4 rounded-md border border-stroke bg-white p-3 dark:border-strokedark dark:bg-boxdark shadow shadow-2xl">
                 Practice Time Achievements
               </div>
           
               {/* Best Streak Box */}
-              <div className="col-span-1 row-span-1 sm:col-span-1 xl:col-span-3 xl:row-span-1 bg-gray-200 p-4 rounded-md border border-stroke bg-white dark:border-strokedark dark:bg-boxdark shadow shadow-2xl">
+              <div className="sm:col-span-1 xl:col-span-1 xl:row-span-3 bg-gray-200 p-4 rounded-md border border-stroke bg-white dark:border-strokedark dark:bg-boxdark shadow shadow-2xl">
+                <div className='w-full text-center font-semibold text-lg mb-4 sm:mb-0'>
                 Best Streak
+                </div>
+                <div className='flex flex-col w-full h-4/6 justify-center items-center'>
+                <StreakCounter count={hobby.bestStreak} />
+                </div>
+                <div className='text-center text-lg'>
+                    Your best streak so far is {hobby.bestStreak} days!
+                </div>
               </div>
           
             </div>
